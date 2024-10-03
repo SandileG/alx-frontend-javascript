5f52f766d1a87c58b06b04e6d599723561b6aae1
SandileG <sandilegraham@gmail.com>
2024-10-03 21:55:33 +0000

    3-default-parameter.js

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+  import getSumOfHoods from './3-default-parameter.js';
+
+  console.log(getSumOfHoods(34));          // Outputs: 142
+  console.log(getSumOfHoods(34, 3));       // Outputs: 56
+  console.log(getSumOfHoods(34, 3, 4));    // Outputs: 41
