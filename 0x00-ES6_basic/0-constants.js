c3bf35057b77c84681bac161ff08803233cb8886
SandileG <sandilegraham@gmail.com>
2024-10-05 23:26:59 +0000

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-// 0-constans.js
+export function taskFirst() {
+  const task = 'I prefer const when I can.'; 
+  return task;
+}
 
-  export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
+export function getLast() {
+  return ' is okay';
+}
 
-  export function getLast() {
-    return ' is okay';
-  }
+export function taskNext() {
+  let combination = 'But sometimes let'; 
+  combination += getLast();
 
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  return combination;
+}
