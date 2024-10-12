14400e1835798f7408aacc38e5e1fc697a5cf4ae
SandileG <sandilegraham@gmail.com>
2024-10-12 11:37:42 +0000

    4-user-promise.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,6 @@
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName, // using shorthand property name
+    lastName, // using shorthand property name
+  });
+}
