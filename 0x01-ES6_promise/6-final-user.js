b7c36eb8b0dbcfb831110f0d688c2d62461a5f43
SandileG <sandilegraham@gmail.com>
2024-10-12 12:39:44 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,9 +6,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   // Use Promise.allSettled to wait for both promises to settle
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
+    // Optional: Log the results for debugging
+    console.log(results);
+    return results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    }));
+  });
 }
