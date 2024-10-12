9efe8cf9e53bc46eb90901a24655b93a0aaa4a78
SandileG <sandilegraham@gmail.com>
2024-10-12 19:29:49 +0000

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+// 100-await.js
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    // Await both promises concurrently
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    
+    // Return the result object
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    // In case of an error, return an empty object
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
